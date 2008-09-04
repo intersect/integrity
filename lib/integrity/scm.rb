53d44966318f0370ba19b89c7cebdd33adeae0c9
Scott Taylor <scott@railsnewbie.com>
2008-09-04 01:24:42 -0400

    * Fix parsing support for private git repos. * More flexible parsing of URLs

--- lib/integrity/scm.rb
+++ lib/integrity/scm.rb
@@ -1,12 +1,33 @@
 module Integrity
   module SCM
+    class SCMUnknownError < StandardError; end
     class << self
       def new(uri, *args)
-        scm = uri.scheme
-        klass = scm.capitalize.gsub(/_(.)/) { $1.upcase }
-        const_get(klass).new(uri, *args)
-      rescue LoadError, NameError
-        raise "could not find any SCM named `#{uri.scheme}'"
+        scm_class_for(uri).new(uri, *args)
+      end
+      
+    private
+      
+      def scm_class_for(string)
+        UrlParser.parse(string)
+      end
+    end
+    
+    class UrlParser
+      def self.parse(an_object)
+        new(an_object).parse
+      end
+      
+      def initialize(an_object)
+        @string = an_object.to_s
+      end
+      
+      def parse
+        if @string =~ /git/i
+          Integrity::SCM::Git
+        else
+          raise SCMUnknownError, "could not find any SCM based on string '#{@string}'"
+        end
       end
     end
   end
--- spec/scm_spec.rb
+++ spec/scm_spec.rb
@@ -1,29 +1,43 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
-describe Integrity::SCM do
-  describe 'when loading an SCM adapter' do
-    before(:each) do
-      @uri = Addressable::URI.parse('git://github.com/foca/integrity.git')
-    end
+module Integrity
+  describe SCM do
+    describe 'when loading an SCM adapter' do
+      before(:each) do
+        @uri = Addressable::URI.parse('git://github.com/foca/integrity.git')
+      end
 
-    it 'should instantiate the adapter with the given options' do
-      build = mock('build model')
-      Integrity::SCM::Git.should_receive(:new).
-        with(@uri, 'master', build)
-      Integrity::SCM.new(@uri, 'master', build)
-    end
+      it 'should instantiate the adapter with the given options' do
+        build = mock('build model')
+        Integrity::SCM::Git.should_receive(:new).with(@uri, 'master', build)
+        Integrity::SCM.new(@uri, 'master', build)
+      end
 
-    it "should raise an error if the handler isn't defined into the file" do
-      Integrity::SCM.send(:remove_const, :Git)
-      lambda do
-        Integrity::SCM.new(@uri)
-      end.should raise_error(RuntimeError, "could not find any SCM named `git'")
+      it "should raise an error if the handler can't be found" do
+        lambda do
+          Integrity::SCM.new("foo/bar")
+        end.should raise_error(SCM::SCMUnknownError, "could not find any SCM based on string 'foo/bar'")
+      end
     end
-
-    it "should raise an error if the file defining the handler can't be loaded" do
-      lambda do
-        Integrity::SCM.new(@uri)
-      end.should raise_error(RuntimeError, "could not find any SCM named `git'")
+    
+    describe SCM::UrlParser do
+      before :each do
+        @parser = SCM::UrlParser
+      end
+      
+      it "should return Integrity::SCM::Git if the url contains git" do
+        @parser.parse("git://fo/bar").should == Integrity::SCM::Git
+      end
+      
+      it "should return Integrity::SCM::Git if the url contains 'GIT'" do
+        @parser.parse("GIT://fo/bar").should == Integrity::SCM::Git
+      end
+      
+      it "should raise an SCMUnknownError if the url doesn't contain git" do
+        lambda { 
+          @parser.parse("foo")
+        }.should raise_error(SCM::SCMUnknownError)
+      end
     end
   end
 end
