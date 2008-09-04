dab8757b025d369db25f1d379b327f3f1bd6d030
Scott Taylor <scott@railsnewbie.com>
2008-09-04 01:03:58 -0400

    * Use explicit metaclass.  It's more flexible

--- lib/integrity/scm.rb
+++ lib/integrity/scm.rb
@@ -1,11 +1,13 @@
 module Integrity
   module SCM
-    def self.new(uri, *args)
-      scm = uri.scheme
-      klass = scm.capitalize.gsub(/_(.)/) { $1.upcase }
-      const_get(klass).new(uri, *args)
-    rescue LoadError, NameError
-      raise "could not find any SCM named `#{uri.scheme}'"
+    class << self
+      def new(uri, *args)
+        scm = uri.scheme
+        klass = scm.capitalize.gsub(/_(.)/) { $1.upcase }
+        const_get(klass).new(uri, *args)
+      rescue LoadError, NameError
+        raise "could not find any SCM named `#{uri.scheme}'"
+      end
     end
   end
 end
